f231b837f33f8ca06816671fd299616075369250
facudam <facundo89caceres@gmail.com>
2023-02-19 19:44:17 -0300

    Desktop Nav styles completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,17 +6,17 @@ export const Nav = () => {
   return (
     <div className='nav'>
         <div className='nav__top'>
-            <div>
+            <div className='nav__top-marca'>
                 <a href='#'><img className='logo' src={ logo } alt='logo de mercado libre' /></a>
-                <form>
-                    <input type='search' placeholder='Buscar Productos, Marcas y más...'/>
-                    <div>
+                <form className='form'>
+                    <input type='search' placeholder='Buscar productos, marcas y más...'/>
+                    <div className='form__img-container'>
                         <img className='lupa' src={ lupa } alt='lupa-search' />
                     </div>
                 </form>
             </div>
             
-            <div>
+            <div className='propaganda'>
                 <a href=''><img src='https://http2.mlstatic.com/D_NQ_887100-MLA50801817839_072022-OO.webp' alt='promo-disney' /></a>
             </div>
         </div>
