ba93e8e8eace17c28fdc60228ba752c1f09c753f
facudam <facundo89caceres@gmail.com>
2023-02-28 19:23:50 -0300

    Slider buttons styles added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -15,7 +15,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
 
 export default () => {
