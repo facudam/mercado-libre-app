15a16585bf2b559a9d06fafb1ccfc05fd1bb894c
facudam <facundo89caceres@gmail.com>
2023-03-12 19:57:15 -0300

    Medios de pago modal styles corrected

--- src/modals/MediosDePago.jsx
+++ src/modals/MediosDePago.jsx
@@ -41,7 +41,7 @@ export const MediosDePago = () => {
     }, [])
 
     const { setMediosDePagoIsActive } = useContext(MeliContext);
-    const [ isOnScroll, setIsOnScroll ] = useState(false)
+    
 
     return ReactDOM.createPortal(
         <div>
@@ -52,91 +52,95 @@ export const MediosDePago = () => {
                     <button onClick={() => setMediosDePagoIsActive(false)}>x</button>
                 </div>
 
-                <p className="payment-modal-subtitle">Podés pagar tus compras con cualquiera de estos medios. Es rápido y seguro, siempre.</p>
-                <div className="separator"></div>
-                <div className="mercado-credito-section">
-                    <img src={ mercadoCreditoLogo } alt='mercado crédito logo' />
-                    <div className="mercado-credito-section-paragraph">
-                        <p>¡Compra en cuotas sin tarjeta!</p>
-                        <p>Activa Mercado Crédito, es 100% online y sin costos de mantenimiento.</p>
+                <div className="info-modal">
+                    <p className="payment-modal-subtitle">Podés pagar tus compras con cualquiera de estos medios. Es rápido y seguro, siempre.</p>
+                    <div className="separator"></div>
+                    <div className="mercado-credito-section">
+                        <img src={ mercadoCreditoLogo } alt='mercado crédito logo' />
+                        <div className="mercado-credito-section-paragraph">
+                            <p>¡Compra en cuotas sin tarjeta!</p>
+                            <p>Activa Mercado Crédito, es 100% online y sin costos de mantenimiento.</p>
+                        </div>
+                        <button className="mercado-credito-section-btn">Activar ahora</button>
                     </div>
-                    <button className="mercado-credito-section-btn">Activar ahora</button>
-                </div>
-                <div className="separator"></div>
-                <div className="tarjetas-section">
-                    <img src={ tarjetaCredito } alt='tarjeta de crédito' />
-                    <div className="tarjetas__info">
-                        <div className="tarjetas-paragraphs">
-                            <p>Tarjetas de crédito en hasta 24 cuotas*</p>
-                            <p>Acreditación instantánea.</p>
-                        </div>
-                        <div className="tarjetas-imgs">
-                            <img src={ visa } alt='tarjeta visa' />
-                            <img src={ cabal } alt='tarjeta cabal' />
-                            <img src={ shopping } alt='tarjeta shopping' />
-                            <img src={ cencosud } alt='tarjeta cencosud' />
-                            <img src={ dinersclub } alt='tarjeta dinersclub' />
-                            <img src={ argencard } alt='tarjeta argencard' />
-                            <img src={ cmr } alt='tarjeta cmr' />
-                            <img src={ mastercard } alt='tarjeta mastercard' />
-                            <img src={ naranja } alt='tarjeta naranja' />
-                            <img src={ american } alt='tarjeta american express' />
-                        </div>
-                        <p>*La cantidad de cuotas puede variar según la tarjeta.</p>
-                        <a href="">Ver promociones en cuotas</a>
-                    </div>
-                </div>
-                <div className="separator"></div>
-                <div className="tarjetas-section">
-                    <img src={ debito } alt='tarjeta de débito' />
-                    <div className="tarjetas__info">
-                        <div className="tarjetas-paragraphs">
-                            <p>Tarjetas de débito</p>
-                            <p>Acreditación instantánea</p>
-                        </div>
-                        <div className="tarjetas-imgs">
-                            <img src={ maestroDebito } alt='Maestro Débito' />
-                            <img src={ masterDebito } alt='Mastercard Débito' />
-                            <img src={ cabalDebito } alt='cabal Débito' />
-                            <img src={ visaDebito}  alt='visa Débito' />
-                        </div>
-                    </div>
-                </div>
-                <div className="separator"></div>
-                <div className="tarjetas-section">
-                    <img src={ efectivo } alt='efectivo' />
-                    <div className="tarjetas__info">
-                        <div className="tarjetas-paragraphs">
-                            <p>Efectivo</p>
-                            <p>Es muy simple: cuando termines tu compra, te daremos las instrucciones para que sepas cómo y dónde pagarla.</p>
-                        </div>
-                        <div className="tarjetas-imgs">
-                            <div>
-                                <img src={ pagoFacil } alt='pago facil' />
-                                <p>Acreditación en 1 día hábil.</p>
-                            </div>
-                            <div>
-                                <img src={ rapipago } alt='rapipago' />
+                    <div className="separator"></div>
+                    <div className="tarjetas-section">
+                        <img src={ tarjetaCredito } alt='tarjeta de crédito' />
+                        <div className="tarjetas__info">
+                            <div className="tarjetas-paragraphs">
+                                <p>Tarjetas de crédito en hasta 24 cuotas*</p>
                                 <p>Acreditación instantánea.</p>
                             </div>
+                            <div className="tarjetas-imgs">
+                                <img src={ visa } alt='tarjeta visa' />
+                                <img src={ cabal } alt='tarjeta cabal' />
+                                <img src={ shopping } alt='tarjeta shopping' />
+                                <img src={ cencosud } alt='tarjeta cencosud' />
+                                <img src={ dinersclub } alt='tarjeta dinersclub' />
+                                <img src={ argencard } alt='tarjeta argencard' />
+                                <img src={ cmr } alt='tarjeta cmr' />
+                                <img src={ mastercard } alt='tarjeta mastercard' />
+                                <img src={ naranja } alt='tarjeta naranja' />
+                                <img src={ american } alt='tarjeta american express' />
+                            </div>
+                            <p>*La cantidad de cuotas puede variar según la tarjeta.</p>
+                            <a href="">Ver promociones en cuotas</a>
+                        </div>
+                    </div>
+                    <div className="separator"></div>
+                    <div className="tarjetas-section">
+                        <img src={ debito } alt='tarjeta de débito' />
+                        <div className="tarjetas__info">
+                            <div className="tarjetas-paragraphs">
+                                <p>Tarjetas de débito</p>
+                                <p>Acreditación instantánea</p>
+                            </div>
+                            <div className="tarjetas-imgs">
+                                <img src={ maestroDebito } alt='Maestro Débito' />
+                                <img src={ masterDebito } alt='Mastercard Débito' />
+                                <img src={ cabalDebito } alt='cabal Débito' />
+                                <img src={ visaDebito}  alt='visa Débito' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="separator"></div>
+                    <div className="tarjetas-section">
+                        <img src={ efectivo } alt='efectivo' />
+                        <div className="tarjetas__info">
+                            <div className="tarjetas-paragraphs">
+                                <p>Efectivo</p>
+                                <p>Es muy simple: cuando termines tu compra, te daremos las instrucciones para que sepas cómo y dónde pagarla.</p>
+                            </div>
+                            <div className="tarjetas-imgs">
+                                <div>
+                                    <img src={ pagoFacil } alt='pago facil' />
+                                    <p>Acreditación en 1 día hábil.</p>
+                                </div>
+                                <div>
+                                    <img src={ rapipago } alt='rapipago' />
+                                    <p>Acreditación instantánea.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="separator"></div>
+                    <div className="tarjetas-section">
+                        <img src={ dineroEnCuenta } alt='dinero en celular' />
+                        <div className="tarjetas__info">
+                            <div className="tarjetas-paragraphs">
+                                <p>Dinero en tu cuenta de Mercado Pago</p>
+                                <p>Al finalizar tu compra, pagás con el dinero disponible en tu cuenta. Podés ingresar dinero a Mercado Pago por Débito inmediato, transferencia bancaria o en efectivo.</p>
+                                <p>Acreditación instantánea.</p>
+                            </div>
+                            <div className="tarjetas-imgs">
+                                <img src={ mercadoPago } alt='mercado pago logo' />
+                            </div>
+                            <a href="">Conocé más</a>
                         </div>
                     </div>
                 </div>
-                <div className="separator"></div>
-                <div className="tarjetas-section">
-                    <img src={ dineroEnCuenta } alt='dinero en celular' />
-                    <div className="tarjetas__info">
-                        <div className="tarjetas-paragraphs">
-                            <p>Dinero en tu cuenta de Mercado Pago</p>
-                            <p>Al finalizar tu compra, pagás con el dinero disponible en tu cuenta. Podés ingresar dinero a Mercado Pago por Débito inmediato, transferencia bancaria o en efectivo.</p>
-                            <p>Acreditación instantánea.</p>
-                        </div>
-                        <div className="tarjetas-imgs">
-                            <img src={ mercadoPago } alt='mercado pago logo' />
-                        </div>
-                        <a href="">Conocé más</a>
-                    </div>
-                </div>
+
+                
             </div>
         </div>,
         document.getElementById('portal')
